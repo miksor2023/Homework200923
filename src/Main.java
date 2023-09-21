e1b32d7fcad2d8068e366e5546ea6c479e4b4a82
miksor2023 <miksor@yandex.ru>
2023-09-21 20:21:50 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //
+        //задача 1
+        System.out.println("\nЗадача1");
+        checkLeapYear(2000);
     }
+    public static void checkLeapYear(int year) {
+        if (year >= 1584 && year%4 == 0 && (year%100 != 0 || year%400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
 }
+
