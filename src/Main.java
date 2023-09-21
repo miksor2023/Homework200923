ca382f9263ab7e8851aa9db2e544fc766e42c6bf
miksor2023 <miksor@yandex.ru>
2023-09-21 20:27:21 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
     }
     public static void checkLeapYear(int year) {
         if (year >= 1584 && year%4 == 0 && (year%100 != 0 || year%400 == 0)) {
-            System.out.println(year + " год является високосным");
+            System.out.println(year + " год — високосный год");
         } else {
-            System.out.println(year + " год не является високосным");
+            System.out.println(year + " год — невисокосный год");
         }
     }
 
