971023d8f1e1bc5727e0f354d077f4628e7d1d8c
miksor2023 <miksor@yandex.ru>
2023-09-21 20:34:18 +0300

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         //задача 1
         System.out.println("\nЗадача1");
         checkLeapYear(2000);
+        //
+        //задача 2
+        System.out.println("\nЗадача 2");
+        chooseAppVersion(1, 2010);
     }
     public static void checkLeapYear(int year) {
         if (year >= 1584 && year%4 == 0 && (year%100 != 0 || year%400 == 0)) {
@@ -12,6 +16,18 @@ public class Main {
             System.out.println(year + " год — невисокосный год");
         }
     }
+    public static void chooseAppVersion(int clientOS, int clientDeviceYear) {
+        if (clientDeviceYear >= 2015 && clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear >= 2015 && clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientDeviceYear < 2015 && clientOS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+
 
 }
 
